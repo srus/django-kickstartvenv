def4b622fc97c6fcbd557ce6f8c844e38b1c6b04
Sergio Rus <geiros@gmail.com>
2015-02-11 12:19:36 +0100

    Add error checking

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Current path
+proj_path="`pwd`"
+
 # Some colors
 txtrst='\e[0m'     # text reset
 txtylw='\e[0;33m'  # regular yellow
@@ -14,16 +17,13 @@ echo -en "${txtylw}Name of Django project: ${txtrst}"
 read django_name
 echo ""
 
-# Save current path
-proj_path="`pwd`"
-
 # Check if dir exists
-if [[ ! -d "conf" ]]; then
-    mkdir "conf"
+if [[ ! -d "${proj_path}/conf" ]]; then
+    mkdir "${proj_path}/conf" || { echo "FATAL: Could not create 'conf' directory"; exit 1; }
 fi
 
 # Build virtualenv environment
-source /usr/local/bin/virtualenvwrapper.sh
+source /usr/local/bin/virtualenvwrapper.sh || { echo "FATAL: Could not find '/usr/local/bin/virtualenvwrapper.sh'"; exit 1; }
 mkvirtualenv "$venv_name"
 
 # Set and activate virtualenv postactivate script
@@ -33,7 +33,7 @@ p="${proj_path//\//\\/}"  # escape forward slashes
 sed -i "s/{{path}}/$p/g" postactivate
 mv -f postactivate "${WORKON_HOME}/${venv_name}/bin/postactivate"
 deactivate
-source /usr/local/bin/virtualenvwrapper.sh
+source /usr/local/bin/virtualenvwrapper.sh || { echo "FATAL: Could not find '/usr/local/bin/virtualenvwrapper.sh'"; exit 1; }
 workon "$venv_name"
 
 echo ""
@@ -41,14 +41,14 @@ echo -e "${txtgrn}Installing Python packages...${txtrst}"
 echo ""
 
 # Install Python packages
-pip install -U -r requirements/development.txt
+pip install -U -r "${proj_path}/requirements/development.txt" || { echo "FATAL: Could not install Python packages"; exit 1; }
 
 echo ""
 echo -e "${txtgrn}Configuring IPython...${txtrst}"
 echo ""
 
 # Set IPython default profile
-ipython profile create
+ipython profile create || { echo "FATAL: Could not create IPython profile"; exit 1; }
 ln -sf "${proj_path}/boot/ipython/ipython_config.py" "${proj_path}/conf/ipython/profile_default/ipython_config.py"
 
 # Run migrations or create a new Django project
